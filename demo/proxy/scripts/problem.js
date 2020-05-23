198d21d839efbe4c842622adfa184a6b0e785441
Piotr Kowalski <piecioshka@gmail.com>
2020-05-23 11:55:40 +0200

    Import demo for Proxy Pattern

--- demo/proxy/scripts/problem.js
+++ demo/proxy/scripts/problem.js
@@ -1,3 +1,24 @@
 // Pattern: Proxy
 
 console.log('%cFile: problem.js', 'color: red');
+
+{
+    class Request {
+        static async makeRequest(url) {
+            const response = await fetch(url);
+            return response.json();
+        }
+    }
+
+    async function main() {
+        const url = 'https://fakes.herokuapp.com/videos';
+        const data1 = await Request.makeRequest(url);
+        console.log(data1);
+        const data2 = await Request.makeRequest(url);
+        console.log(data2);
+    }
+
+    main();
+
+    // How to add Cache?
+}
--- demo/proxy/scripts/solution.js
+++ demo/proxy/scripts/solution.js
@@ -1,3 +1,37 @@
 // Pattern: Proxy
 
 console.log('%cFile: solution.js', 'color: green');
+
+{
+    class RequestWithCache {
+        static cache = new Map();
+
+        static async makeRequest(url) {
+            let response = RequestWithCache.cache.get(url);
+
+            if (!response) {
+                response = await Request.makeRequest(url);
+                RequestWithCache.cache.set(url, response);
+            }
+
+            return response;
+        }
+    }
+
+    class Request {
+        static async makeRequest(url) {
+            const response = await fetch(url);
+            return response.json();
+        }
+    }
+
+    async function main() {
+        const url = 'https://fakes.herokuapp.com/videos';
+        const data1 = await RequestWithCache.makeRequest(url);
+        console.log(data1);
+        const data2 = await RequestWithCache.makeRequest(url);
+        console.log(data2);
+    }
+
+    main();
+}
