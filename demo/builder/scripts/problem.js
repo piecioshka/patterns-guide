5f831cb36b5ea73069c3fcc036f99ac6c7a87ef9
Piotr Kowalski <piecioshka@gmail.com>
2020-05-23 11:55:28 +0200

    Import demo for Builder Pattern

--- demo/builder/scripts/problem.js
+++ demo/builder/scripts/problem.js
@@ -1,3 +1,20 @@
 // Pattern: Builder
 
 console.log('%cFile: problem.js', 'color: red');
+
+{
+    class Person {
+        constructor(name, surname, age, weight) {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.weight = weight;
+        }
+        displayAge() {
+            console.log(this.age);
+        }
+    }
+
+    const p = new Person('John', 'Doe', 58, 69);
+    p.displayAge() // ??? 58 or 69
+}
--- demo/builder/scripts/solution.js
+++ demo/builder/scripts/solution.js
@@ -1,3 +1,49 @@
 // Pattern: Builder
 
 console.log('%cFile: solution.js', 'color: green');
+{
+    class PersonBuilder {
+        constructor(name, surname) {
+            this.name = name;
+            this.surname = surname;
+        }
+        setAge(age) {
+            this.age = age;
+            return this;
+        }
+        setWeight(weight) {
+            this.weight = weight;
+            return this;
+        }
+        build() {
+            return new Person(
+                this.name,
+                this.surname,
+                this.age,
+                this.weight
+            );
+        }
+    }
+
+    class Person {
+        constructor(name, surname, age, weight) {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.weight = weight;
+        }
+        displayAge() {
+            console.log(this.age);
+        }
+    }
+
+    // const p = new Person('John', 'Doe', 58, 69);
+    // p.displayAge() // ??? 58 or 69
+
+    new PersonBuilder('John', 'Doe')
+        .setAge(58)
+        .setWeight(69)
+        .build()
+        // Next method is from Person Class
+        .displayAge() // 58!
+}
