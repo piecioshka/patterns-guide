509e40d58353537fd3fd01a219693fcffb0336a5
Piotr Kowalski <piecioshka@gmail.com>
2020-05-23 15:15:45 +0200

    Import demo for Adapter Pattern

--- demo/adapter/scripts/problem.js
+++ demo/adapter/scripts/problem.js
@@ -1,3 +1,20 @@
 // Pattern: Adapter
 
 console.log('%cFile: problem.js', 'color: red');
+
+{
+    class Person {
+        constructor(name_surname) {
+            this.name_surname = name_surname;
+        }
+        getName() {
+            return this.name_surname;
+        }
+    }
+
+    const p = new Person('Piotr Kowalski');
+    const name = p.getName();
+    console.log({ name });
+
+    // How to pass name and surname independently?
+}
--- demo/adapter/scripts/solution.js
+++ demo/adapter/scripts/solution.js
@@ -1,3 +1,29 @@
 // Pattern: Adapter
 
 console.log('%cFile: solution.js', 'color: green');
+{
+    class Person {
+        constructor(name_surname) {
+            this.name_surname = name_surname;
+        }
+        getName() {
+            return this.name_surname;
+        }
+    }
+
+    class PersonAdapter {
+        constructor(name, surname) {
+            this.person = new Person(
+                `${name} ${surname}`
+            )
+        }
+        getName() {
+            return this.person.getName();
+        }
+    }
+
+    const pa = new PersonAdapter('Piotr', 'Kowalski');
+    const name = pa.getName(); // 'Piotr Kowalski'
+
+    console.log({ name });
+}
